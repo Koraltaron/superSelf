bfc0e8b7f30fee6050285958e98fa740f90e2f90
Clément Praud <clement.praud6@gmail.com>
2024-12-06 11:04:20 +0100

    remove fetch and useEffect on App.tsx since the loader is doing the job

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,13 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import "./style/globals.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import logo from "/logo.png";
 import heartPink from "/pinkHeart.png";
-import type { quoteFetchDataTypes } from "./types/quote-fetch-types";
 import useTheme from "./utils/useTheme";
 
 function App() {
-  const [apiQuoteData, setApiQuoteData] = useState<quoteFetchDataTypes>();
   const [srcHero, setSrcHero] = useState<string>("");
-  const category = "architecture";
-  const options = {
-    method: "GET",
-    headers: { "x-api-key": `${import.meta.env.VITE_NINJA_API_KEY}` },
-  };
-  const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
-  useEffect(() => {
-    fetch(url, options)
-      .then((response) => response.json())
-      .then((data) => {
-        setApiQuoteData(data);
-      });
-  }, [url]);
-
-  console.warn(apiQuoteData);
 
   const { theme, setTheme } = useTheme();
   console.warn(theme);
